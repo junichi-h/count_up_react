5c666a527bab90e147529cd3e0332c787fc4aee9
Junichi Honda <junichi.hnd@gmail.com>
2018-11-21 10:04:08 +0900

    importの記載順番変更

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,10 +3,9 @@ import 'ress/ress.css';
 import '@babel/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import './bootstrap';
-// import Button from './components/atoms/button';
-import { Provider } from 'react-redux';
 import store from './store';
 import App from './components/app';
 
